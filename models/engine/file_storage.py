096ae5ce3f7df61cd4a7a3de41c1e5f1d60a1436
Jocseh07 <josephmkaranja07@gmail.com>
2024-05-27 17:02:03 +0300

    pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ Contains the FileStorage class
 """
 
 import json
+
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -55,7 +56,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except (FileNotFoundError, ValueError, KeyError, TypeError):
             pass
 
     def delete(self, obj=None):
