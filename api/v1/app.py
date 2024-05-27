e89fec47246c03f3017bb1efc5ef590ee0682f5e
Joseph Muturi <129220929+Jocseh07@users.noreply.github.com>
2024-05-27 20:32:46 +0300

    error

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 
 from os import getenv
 
-from flask import Flask
+from flask import Flask, jsonify
 
 from api.v1.views import app_views
 from models import storage
@@ -17,6 +17,12 @@ app.register_blueprint(app_views)
 def close_storage(exception):
     """Close storage."""
     storage.close()
+
+
+@app.errorhandler(404)
+def error_404(error):
+    """Error 404."""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
