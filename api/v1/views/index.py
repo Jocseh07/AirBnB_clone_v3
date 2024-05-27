4d89cc65230f816da1f64bb3b3139bf360d8e23a
Jocseh07 <josephmkaranja07@gmail.com>
2024-05-27 18:00:09 +0300

    4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,14 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Return OK status."""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Return stats."""
