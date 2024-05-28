203b2d74aa1edfac8dd758162775ca067a0e2342
Joseph Muturi <129220929+Jocseh07@users.noreply.github.com>
2024-05-28 08:54:42 +0300

    file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -72,10 +72,19 @@ class FileStorage:
 
     def get(self, cls, id):
         """retrieve one object"""
+        # if cls is not None:
+        #     new_dict = {}
+        #     for key, value in self.__objects.items():
+        #         if cls == value.__class__ or cls == value.__class__.__name__:
+        #             new_dict[key] = value
+        #     return new_dict
+        # return self.__objects
         if cls is None or id is None:
             return None
-        key = cls + '.' + id
-        return self.__objects.get(key)
+        total = self.all(cls)
+        for key, value in total:
+            if id is value:
+                return total[key]
 
     def count(self, cls=None):
         """Count the number of objects in storage"""
