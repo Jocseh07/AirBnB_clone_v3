4fcf3ae3f4d6d6bb9d3f70a139dce3ab528bdcf2
Joseph Muturi <129220929+Jocseh07@users.noreply.github.com>
2024-05-29 04:30:16 +0300

    more

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -84,7 +84,8 @@ def update_review(review_id):
     if not data:
         return jsonify({"error": "Not a JSON"}), 400
     for key, value in data.items():
-        if key not in ['id', 'created_at', 'updated_at']:
+        if key not in ['id', 'created_at', 'updated_at',
+                       'place_id', 'user_id']:
             setattr(review, key, value)
     review.save()
     return jsonify(review.to_dict()), 200
